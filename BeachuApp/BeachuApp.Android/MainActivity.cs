5e3e3c72a2e82f6df21a375855e4bebfe49f0053
Luca Panariello <l.panariello@campus.uniurb.it>
2019-12-31 21:36:08 +0100

    Risolto crash

--- BeachuApp/BeachuApp.Android/MainActivity.cs
+++ BeachuApp/BeachuApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 namespace BeachuApp.Droid
 {
     [Activity(Label = "BeachuApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
